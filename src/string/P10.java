8bb162ea7713ce45ff3c7067b0f63ee676214ed8
VanshikaMahle <vanshika.mahle_cs18@gla.ac.in>
2019-10-03 01:20:30 +0530

    init commit;

---
+++ src/string/P10.java
@@ -0,0 +1,19 @@
+package string;
+
+import java.util.Scanner;
+
+public class P10 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        char c = s.nextLine().charAt(0);
+        int l = s1.length();
+        System.out.println("Output: ");
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+            if (ch == c) {
+                System.out.println(i);
+            }
+        }
+    }
+}
